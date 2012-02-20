8e7e593b1b01b7790192c3ca19350e95020649ac
Romain Hinfray <romain.hinfray@echoes-tech.com>
2012-02-20 15:35:41 +0100

    ajout du projet de tests unitaires

---
+++ tests/include/TestConnectionTCP.h
@@ -0,0 +1,18 @@
+#ifndef TESTCONNECTIONTCP_H
+#define TESTCONNECTIONTCP_H
+
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <iostream>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/bind.hpp>
+#include "ConnectionTCP.h"
+
+class TestConnectionTCP
+{
+    public:
+    protected:
+    private:
+};
+
+#endif // TESTCONNECTIONTCP_H
