25faee7cc172dd8cfc183ef2f72130d6253ce3a2
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2013-02-05 15:37:53 +0100

    Conf file for puppet

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
     boost::program_options::notify(vm);    
-    std::string confFile = "engine.conf";
+    std::string confFile;
     
     if (vm.count("help")) {
         std::cout << desc << "\n";
