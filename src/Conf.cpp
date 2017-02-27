b4c642caaa29fd5f44f4b6eaff8c25ffae8ab928
Thomas <thomas@echoes.fr>
2017-02-27 16:14:19 +0100

    opensourcing : changing headers for cpp files

--- src/Conf.cpp
+++ src/Conf.cpp
@@ -1,13 +1,25 @@
-/* 
- * Engine Configuration
- * @author ECHOES Technologies (FPO)
- * @date 03/06/2013
- * 
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO ECHOES TECHNOLOGIES SAS
- * AND MAY NOT BE REPRODUCED, PUBLISHED OR DISCLOSED TO OTHERS WITHOUT
- * COMPANY AUTHORIZATION.
- * 
- * COPYRIGHT 2013 BY ECHOES TECHNOLGIES SAS
+/**
+ * @file Conf.cpp
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Alert - Engine is a part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  * 
  */
 
@@ -315,4 +327,3 @@ unsigned short Conf::getCriticity() const
 {
     return m_criticity;
 }
-
